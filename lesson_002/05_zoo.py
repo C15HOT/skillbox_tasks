6dcf0bde0e945ed4c7ac1d7f91ba26f903c21fdf
Максим <bi4ok23@gmail.com>
2020-09-28 10:42:44 +0300

    lesson 002:зачёт

--- lesson_002/05_zoo.py
+++ lesson_002/05_zoo.py
@@ -10,12 +10,11 @@ zoo = ['lion', 'kangaroo', 'elephant', 'monkey']
 zoo.insert(1, 'beer')
 print(zoo)
 
-
 # добавьте птиц из списка birds в последние клетки зоопарка
 birds = ['rooster', 'ostrich', 'lark']
 #  и выведите список на консоль
-zoo.extend([birds[0],birds[1],birds[2]])
-
+# zoo.extend([birds[0],birds[1],birds[2]])
+zoo.extend(birds)
 print(zoo)
 
 # уберите слона
@@ -26,6 +25,7 @@ print(zoo)
 
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
-print('лев сидит в клетке с номером ', zoo.index('lion')+1)
-print('жаворонок сидит в клетке с номером ',zoo.index('lark')+1)
+print('лев сидит в клетке с номером ', zoo.index('lion') + 1)
+print('жаворонок сидит в клетке с номером ', zoo.index('lark') + 1)
 
+#зачёт!
