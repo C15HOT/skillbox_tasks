447613028582b2d98d43986134cb7f57793a791c
C15HOT <wvxp@mail.ru>
2020-10-10 00:44:37 +0300

    1 часть 3 задания

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -11,7 +11,7 @@
 # Использовать только операторы if/elif/else, можно вложенные
 
 envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+#paper_x, paper_y = 8, 9
 # проверить для
 # paper_x, paper_y = 9, 8
 # paper_x, paper_y = 6, 8
@@ -21,7 +21,12 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if envelop_x >= paper_x and envelop_y >= paper_y:
+    print('Да')
+elif envelop_x >= paper_y and envelop_y >= paper_x:
+    print('Да')
+else:
+    print('Нет')
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
