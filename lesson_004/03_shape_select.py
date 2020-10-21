b693aa5c236ae367aa8aeac5c732b98bf111e5ac
C15HOT <wvxp@mail.ru>
2020-10-21 22:24:47 +0300

    выбор фигур

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -9,6 +9,105 @@ import simple_draw as sd
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
-# TODO здесь ваш код
+def triangle(point, length, angle=0):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=3)
+    v3.draw()
+
+
+
+
+
+def square(point, length, angle=0):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length=length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=3)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 270, length=length, width=3)
+    v4.draw()
+
+
+
+
+
+
+def pentagon(point, length, angle=0):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length=length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 72 * 2, length=length, width=3)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 72 * 3, length=length, width=3)
+    v4.draw()
+
+    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 72 * 4, length=length, width=3)
+    v5.draw()
+
+
+
+
+
+
+def hexagon(point, length, angle=0):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=3)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=3)
+    v4.draw()
+
+    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=3)
+    v5.draw()
+
+    v6 = sd.get_vector(start_point=v5.end_point, angle=angle + 300, length=length, width=3)
+    v6.draw()
+
+
+
+
+
+
+fig = ['1: Треугольник',
+        '2: Квадрат',
+        '3: Пятиугольник',
+        '4: Шестиугольник',
+        ]
+
+point = sd.get_point(300, 300)
+
+
+print('Возможные фигуры:', '\n', '\n'.join(fig), '\n', 'Введите название фигуры')
+
+
+number = int(input())
+if number == 1:
+    triangle(point=point, angle=0, length=200)
+elif number ==2:
+    square(point=point, angle=0, length=200)
+elif number ==3:
+    pentagon(point=point, angle=0, length=200)
+elif number ==4:
+    hexagon(point=point, angle=0, length=200)
+else:
+    print('Вы ввели неверный номер')
 
 sd.pause()
