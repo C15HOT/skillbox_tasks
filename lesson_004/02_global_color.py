510f15f05cc0997265a0667b7c1c44430b109bec
C15HOT <wvxp@mail.ru>
2020-10-23 23:12:09 +0300

    второе переделано

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -3,7 +3,6 @@ import simple_draw as sd
 
 sd.set_screen_size(1200, 800)
 
-
 # Добавить цвет в функции рисования геом. фигур. из упр lesson_004/01_shapes.py
 # (код функций скопировать сюда и изменить)
 # Запросить у пользователя цвет фигуры посредством выбора из существующих:
@@ -17,113 +16,109 @@ sd.set_screen_size(1200, 800)
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
+point_0 = sd.get_point(300, 300)
+point_1 = sd.get_point(100, 100)
+point_2 = sd.get_point(400, 500)
+point_3 = sd.get_point(700, 200)
+
 
 def triangle(point, length, color, angle=0):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color)
+    step = 120
+    end = point
+    for cur_angle in range(0, 360 - step, step):
+        v1 = sd.get_vector(start_point=point, angle=angle + cur_angle, length=length, width=3)
+        v1.draw(color=color)
+        point = v1.end_point
 
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=3)
-    v2.draw(color=color)
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=3)
-    v3.draw(color=color)
-
-
-point_0 = sd.get_point(300, 300)
+    sd.line(start_point=v1.end_point, end_point=end, color=color, width=3)
 
 
 def square(point, length, color, angle=0):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color)
+    step = 90
+    end = point
+    for cur_angle in range(0, 360 - step, step):
+        v1 = sd.get_vector(start_point=point, angle=angle + cur_angle, length=length, width=3)
+        v1.draw(color=color)
+        point = v1.end_point
 
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length=length, width=3)
-    v2.draw(color=color)
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=3)
-    v3.draw(color=color)
-
-    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 270, length=length, width=3)
-    v4.draw(color=color)
-
-
-point_1 = sd.get_point(100, 100)
+    sd.line(start_point=v1.end_point, end_point=end, color=color, width=3)
 
 
 def pentagon(point, length, color, angle=0):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color)
+    step = 72
+    end = point
+    for cur_angle in range(0, 360 - step, step):
+        v1 = sd.get_vector(start_point=point, angle=angle + cur_angle, length=length, width=3)
+        v1.draw(color=color)
+        point = v1.end_point
 
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length=length, width=3)
-    v2.draw(color=color)
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 72 * 2, length=length, width=3)
-    v3.draw(color=color)
-
-    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 72 * 3, length=length, width=3)
-    v4.draw(color=color)
-
-    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 72 * 4, length=length, width=3)
-    v5.draw(color=color)
-
-
-point_2 = sd.get_point(400, 500)
+    sd.line(start_point=v1.end_point, end_point=end, color=color, width=3)
 
 
 def hexagon(point, length, color, angle=0):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color)
+    step = 60
+    end = point
+    for cur_angle in range(0, 360 - step, step):
+        v1 = sd.get_vector(start_point=point, angle=angle + cur_angle, length=length, width=3)
+        v1.draw(color=color)
+        point = v1.end_point
 
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=3)
-    v2.draw(color=color)
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=3)
-    v3.draw(color=color)
-    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=3)
-    v4.draw(color=color)
-
-    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=3)
-    v5.draw(color=color)
-
-    v6 = sd.get_vector(start_point=v5.end_point, angle=angle + 300, length=length, width=3)
-    v6.draw(color=color)
+    sd.line(start_point=v1.end_point, end_point=end, color=color, width=3)
 
 
-point_3 = sd.get_point(700, 200)
+colors = {'1': {'color_name': 'Красный', 'sd_name': sd.COLOR_RED}, '2': {'color_name': 'Оранжевый', 'sd_name': sd.COLOR_ORANGE},
+          '3': {'color_name': 'Желтый', 'sd_name': sd.COLOR_YELLOW}, '4': {'color_name': 'Зеленый', 'sd_name': sd.COLOR_GREEN},
+          '5': {'color_name': 'Циановый', 'sd_name': sd.COLOR_CYAN}, '6': {'color_name': 'Синий', 'sd_name': sd.COLOR_BLUE},
+          '7': {'color_name': 'Фиолетовый', 'sd_name': sd.COLOR_PURPLE}}
 
-list = ['1: Красный',
-        '2: Оранжевый',
-        '3: Желтый',
-        '4: Зеленый',
-        '5: Циановый',
-        '6: Синий',
-        '7: Фиолетовый']
+#  В этом случае удобнее создать словарь следующей структуры
+#  словарь = {'0': {'color_name': 'red', 'sd_name': sd.COLOR_RED},...}
+#  Таким образом для каждого цвета у нас будет свой словарь. И у каждого словаря будут одинаковые ключи
+#  'color_name' и 'sd_name'
+#  Тогда можно будет легко проверить ввод (user_input in словарь)
+#  А если среди ключей есть выбор пользователя - по этому ключу мы получим нужный вложенный словарь
+#  А там все ключи одинаковые, можем получить как название цвета, так и sd_цвет
+print('Возможные цвета:')
+for number,_ in colors.items():
+    print(number, ':', colors[number]['color_name'])
 
-colors = {1: sd.COLOR_RED, 2: sd.COLOR_ORANGE, 3: sd.COLOR_YELLOW, 4: sd.COLOR_GREEN, 5: sd.COLOR_CYAN,
-          6: sd.COLOR_BLUE, 7: sd.COLOR_PURPLE}
-# TODO В этом случае удобнее создать словарь следующей структуры
-# TODO словарь = {'0': {'color_name': 'red', 'sd_name': sd.COLOR_RED},...}
-# TODO Таким образом для каждого цвета у нас будет свой словарь. И у каждого словаря будут одинаковые ключи
-# TODO 'color_name' и 'sd_name'
-# TODO Тогда можно будет легко проверить ввод (user_input in словарь)
-# TODO А если среди ключей есть выбор пользователя - по этому ключу мы получим нужный вложенный словарь
-# TODO А там все ключи одинаковые, можем получить как название цвета, так и sd_цвет
-print('Возможные цвета:', '\n', '\n'.join(list), '\n', 'Введите цвет фигур')
-# TODO Я бы вообще предложил отказаться от связки int+input
-# TODO В словаре можно использовать числа-строки, '1' например
-# TODO И тогда единственное условие, которое нужно будет - есть ли строка user_input среди ключей словаря.
-# TODO И пока её там нет - надо повторять инпут
-number = int(input())
-# TODO Тут хорошо бы реализовать следующие шаги
-# TODO 1) Цикл, который распечатает номера и названия цветов из словаря выше
-# TODO 2) ввод пользователя (без int, тк ключи в словаре у нас строки, а не числа)
-# TODO 3) проверка(лучше в цикле) "если ввод среди ключей словаря" --> то выбранный_цвет = словарь[ввод]['sd_name']
-if number in colors:
-    color = colors.get(number)
+print('Введите цвет фигур')
+#  Я бы вообще предложил отказаться от связки int+input
+#  В словаре можно использовать числа-строки, '1' например
+#  И тогда единственное условие, которое нужно будет - есть ли строка user_input среди ключей словаря.
+#  И пока её там нет - надо повторять инпут
+user_input = input()
+#  Тут хорошо бы реализовать следующие шаги
+#  1) Цикл, который распечатает номера и названия цветов из словаря выше
+#  2) ввод пользователя (без int, тк ключи в словаре у нас строки, а не числа)
+#  3) проверка(лучше в цикле) "если ввод среди ключей словаря" --> то выбранный_цвет = словарь[ввод]['sd_name']
+if user_input in colors:
+    color = colors[user_input]['sd_name']
     triangle(point=point_0, color=color, angle=0, length=200)
     square(point=point_1, color=color, angle=0, length=200)
     pentagon(point=point_2, color=color, angle=0, length=100)
     hexagon(point=point_3, color=color, angle=0, length=100)
 else:
-    print('Вы ввели неверный номер')
+    while user_input not in colors:
+        print('Вы ввели неверный номер')
+        user_input = input()
+        if user_input in colors:
+            color = colors[user_input]['sd_name']
+            triangle(point=point_0, color=color, angle=0, length=200)
+            square(point=point_1, color=color, angle=0, length=200)
+            pentagon(point=point_2, color=color, angle=0, length=100)
+            hexagon(point=point_3, color=color, angle=0, length=100)
+
+
+
+
+
+
+
+
+
+
+
+
 
 sd.pause()
