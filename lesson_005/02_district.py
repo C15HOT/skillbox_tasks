f34b211b69b947053871a9a539951607901d6ef5
C15HOT <wvxp@mail.ru>
2020-10-27 13:28:41 +0300

    район

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,4 +5,16 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+import central_street.house1.room1
+import central_street.house1.room2
+import central_street.house2.room1
+import central_street.house2.room2
+import soviet_street.house1.room1
+import soviet_street.house1.room2
+import soviet_street.house2.room1
+import soviet_street.house2.room2
+
+print('На районе живут: ',','.join(central_street.house1.room1.folks),'\n',','.join(central_street.house1.room2.folks),
+      ','.join(central_street.house2.room1.folks),'\n',','.join(central_street.house2.room2.folks),',',
+','.join(soviet_street.house1.room1.folks),'\n',','.join(soviet_street.house1.room2.folks),',',
+','.join(soviet_street.house2.room1.folks),'\n',','.join(soviet_street.house2.room2.folks))
