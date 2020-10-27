37127710ead8bb3904fbceb56346bf0f55831ad8
C15HOT <wvxp@mail.ru>
2020-10-27 13:49:19 +0300

    бургер

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -14,7 +14,10 @@ import soviet_street.house1.room2
 import soviet_street.house2.room1
 import soviet_street.house2.room2
 
-print('На районе живут: ',','.join(central_street.house1.room1.folks),'\n',','.join(central_street.house1.room2.folks),
-      ','.join(central_street.house2.room1.folks),'\n',','.join(central_street.house2.room2.folks),',',
-','.join(soviet_street.house1.room1.folks),'\n',','.join(soviet_street.house1.room2.folks),',',
-','.join(soviet_street.house2.room1.folks),'\n',','.join(soviet_street.house2.room2.folks))
+print('На районе живут: ', ','.join(central_street.house1.room1.folks), '\n',
+      ','.join(central_street.house1.room2.folks),
+      ','.join(central_street.house2.room1.folks), '\n', ','.join(central_street.house2.room2.folks), ',',
+      ','.join(soviet_street.house1.room1.folks), '\n', ','.join(soviet_street.house1.room2.folks), ',',
+      ','.join(soviet_street.house2.room1.folks), '\n', ','.join(soviet_street.house2.room2.folks))
+#Очень странно что pycharm видит только несколько модулей из общего списка, если не делать папку district источником
+#Соответственно, без этой операции программа не работает.
