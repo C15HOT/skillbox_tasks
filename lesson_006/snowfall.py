3da51f183c4d17e692de151563c0912fae530db5
c15hot <ffuuu1@mail.ru>
2020-11-04 21:42:09 +0300

    исправлено

--- lesson_006/snowfall.py
+++ lesson_006/snowfall.py
@@ -2,18 +2,21 @@ import simple_draw as sd
 N=5
 start_pull_of_y_coordinate = [300]*N
 start_pull_of_x_coordinate = list(range(1,N*100,100))
-
-
+points=[]
+step=10
 def make_snawflake():
     for i in range(N):
-        point = sd.get_point(start_pull_of_x_coordinate[i], start_pull_of_y_coordinate[i])
-        sd.snowflake(center=point, length=50)
+        point=sd.get_point(start_pull_of_x_coordinate[i], start_pull_of_y_coordinate[i])
+        points.append(point)
+
 
 def draw_snowflake():
-
+    for i in range(N):
+        sd.snowflake(center=points[i], length=50)
 
 def shift_snowflake():
-    pass
+    for i in range(N):
+        points[i]=sd.get_point(start_pull_of_x_coordinate[i],start_pull_of_y_coordinate[i]-step)
 
 def number_of_snowflake():
     pass
@@ -21,4 +24,8 @@ def number_of_snowflake():
 def del_snowflake():
     pass
 make_snawflake()
+draw_snowflake()
+shift_snowflake()
+draw_snowflake()
+
 sd.pause()
