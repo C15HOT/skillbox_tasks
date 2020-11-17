3502849e313262d1d6a0b3982265a00ef6fbf73e
c15hot <ffuuu1@mail.ru>
2020-11-17 21:22:22 +0300

    9 парсер

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -23,7 +23,35 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+class Parser:
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.stat = {}
+
+    def collect(self):
+        with open(file=self.filename,mode='r', encoding='utf8') as file:
+            for line in file:
+                self.line_stat(line)
+
+
+    def line_stat(self,line):
+        if 'NOK' in line:
+            key = line[0:17]
+            if key in self.stat:
+                self.stat[key] += 1
+            else:
+                self.stat[key] = 1
+
+    def give_stat(self):
+        with open(file='stat.txt', mode='w+', encoding='utf8') as file:
+            for key, item in self.stat.items():
+
+                file.write(f'{key}]  {item}''\n')
+
+logs = Parser(filename='events.txt')
+logs.collect()
+logs.give_stat()
 
 # После зачета первого этапа нужно сделать группировку событий
 #  - по часам
