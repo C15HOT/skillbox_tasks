539710aca90431b0bdb3d8d88879cf1bc5fef754
c15hot <ffuuu1@mail.ru>
2020-12-25 11:51:09 +0300

    14 3

--- lesson_014/bowling.py
+++ lesson_014/bowling.py
@@ -7,16 +7,28 @@ class State(metaclass=ABCMeta):
         pass
 
 
-class Game:
+class Game():
 
-    def __init__(self):
+    def __init__(self, rules='internal'):
         self.score = 0
-        self.firstthrow = FirstThrow(self)
-        self.secondthrow = SecondThrow(self)
+        self.rules = rules
+        if self.rules == 'internal':
+            self.firstthrow = FirstThrow(self)
+            self.secondthrow = SecondThrow(self)
+        else:
+            self.firstthrow = FirstThrowExt(self)
+            self.secondthrow = SecondThrowExt(self)
+
         self.state = self.firstthrow
         self.frames = 0
         self.firstthrow_score = 0  # запоминание результата первого броска
         self.frame_result = 0
+
+        self.last_result = None
+
+        self.first_throw_result = 0
+        self.second_throw_result = 0
+        self.third_throw_result = 0
 
     def throw(self, result):
         self.state.throw(result)
@@ -81,6 +93,49 @@ class SecondThrow(State):
             raise ValueError(f'Неверно введены данные - счет во фрейме {self.game.frame_result}')
         self.game.frame_result = 0
 
+class FirstThrowExt(State):
+
+    def __init__(self, game):
+        self.game = game
+
+    def throw(self, result):
+        if self.game.last_result is None:
+            if result == 'X':
+                self.game.first_throw_result = 10
+                self.game.frames += 1
+                self.game.last_result = 10
+                self.game.state = self.game.secondthrow
+
+            elif result == '0':
+                raise ValueError(f'Неверно введены данные - {result}')
+            else:
+
+                if result.isdigit():
+                    self.game.first_throw_result += int(result)
+                    self.game.state = self.game.secondthrow
+                elif result == '-':
+                    self.game.state = self.game.secondthrow
+                else:
+                    raise ValueError(f'Неверно введены данные - {result}')
+
+
+
+
+class SecondThrowExt(State):
+
+    def __init__(self, game):
+        self.game = game
+
+    def throw(self, result):
+
+class ThirdThrowext(State):
+
+    def __init__(self, game):
+        self.game = game
+
+    def throw(self, result):
+
+
 
 def get_score(game, game_result):
     for result in game_result:
@@ -98,7 +153,7 @@ if __name__ == '__main__':
     # get_score(game, '5500X332/3/62--XX')  #  сумма очков за один фрейм не должна превышать 9
     #  0 - должен вызывать ошибку (в нашем случае такая информация кодируется через "-")
     # get_score(game, '532X332/3/62--62X')  #  тут 10 и код не работает, почему?
-    get_score(game, '5511X332/3/62--XX')
+    get_score(game, '5311X332/3/62--XX')
     #  сумма очков за один фрейм не должна превышать 9
     #  0 - должен вызывать ошибку (в нашем случае такая информация кодируется через "-")
     #  55 - фрейм с ошибкой, должен быть 5/ если все 10 кеглей сбиты
