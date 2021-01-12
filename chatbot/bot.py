7fa5e6d2f3ece867a17fd5d459e961b190da70a7
C15HOT <wvxp@mail.ru>
2021-01-12 18:34:42 +0300

    15 бот

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -4,6 +4,7 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import logging
 import time
+import handlers
 
 try:
     import settings
@@ -29,6 +30,12 @@ def configure_logging():
     file_handler.setLevel(logging.DEBUG)
 
 
+class UserState:
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        self. context = context or {}
+
 
 class Bot:
     """
@@ -46,7 +53,7 @@ class Bot:
         self.vk = vk_api.VkApi(token=token)
         self.long_poller = VkBotLongPoll(self.vk, self.group_id)
         self.api = self.vk.get_api()
-        self.user_state = dict() # user_id -> user_state
+        self.user_states = dict() # user_id -> user_state
 
     def run(self):
         """
@@ -64,15 +71,70 @@ class Bot:
         send text message
         :param event VkBotMessageEvent object
         """
-        if event.type == VkBotEventType.MESSAGE_NEW:
+        if event.type != VkBotEventType.MESSAGE_NEW:
             log.debug('отправляем сообщения назад')
-            self.api.messages.send(
-                message=event.object.message['text'],
-                random_id=randint(0, 2 ** 20),
-                peer_id=event.object.message['peer_id']
-            )
+            return
+
+        user_id = event.object.message['peer_id']
+        text = event.object.message['text']
+        if user_id in self.user_states:
+
+            text_to_send = self.continue_scenario(user_id, text=text)
         else:
-            log.info('не обработано событие %s', event.type)
+            #search intent
+            for intent in settings.INTENTS:
+                log.debug(f'User gets {intent}')
+                if any(token in text.lower() for token in intent['tokens']):
+                    #run intent
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id, intent['scenario'])
+                    break
+            else:
+                text_to_send = settings.DEFAULT_ANSWER
+
+        self.api.messages.send(
+            message=text_to_send,
+            random_id=randint(0, 2 ** 20),
+            peer_id=user_id
+        )
+
+    def start_scenario(self, user_id, scenario_name):
+        scenario = settings.SCENARIOS[scenario_name]
+        first_step = scenario['first_step']
+        step = scenario['steps'][first_step]
+        text_to_send = step['text']
+        self.user_states[user_id] = UserState(scenario_name=scenario_name, step_name=first_step)
+
+        return text_to_send
+
+
+
+    def continue_scenario(self, user_id, text):
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
+
+        handler = getattr(handlers, step['handler'])
+        if handler(text=text, context=state.context):
+            # next step
+            next_step = steps[step['next_step']]
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                # switch to next step
+                state.step_name = step['next_step']
+            else:
+                # finish
+                log.info('Зарегистрирован {name} {email}'.format(**state.context))
+                self.user_states.pop(user_id)
+
+        else:
+            # retry current step
+            text_to_send = step['failure_text'].format(**state.context)
+
+        return text_to_send
+
 
 
 if __name__ == '__main__':
