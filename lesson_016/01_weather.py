0ef55b1edf22294b121a247fa44764f7f5f65802
C15HOT <wvxp@mail.ru>
2021-02-20 17:30:58 +0300

    16 задание

--- lesson_016/01_weather.py
+++ lesson_016/01_weather.py
@@ -46,3 +46,76 @@
 # Приконнектится по полученному url-пути к базе данных
 # Инициализировать её через DatabaseProxy()
 # https://peewee.readthedocs.io/en/latest/peewee/database.html#dynamically-defining-a-database
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+r = re.compile("[а-яА-Я]+")
+
+
+class WeatherMaker:
+    DAYS = 3
+
+    def __init__(self):
+        self.response = None
+        self.dates = {}
+        self.temperature = []
+        self.states = []
+        self.allstates = []
+        self.moments = ['Ночь', 'Утро', 'День', 'Вечер']
+
+        self.headers = {
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 OPR/40.0.2308.81',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'DNT': '1',
+            'Accept-Encoding': 'gzip, deflate, lzma, sdch',
+            'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4'
+        }
+
+    def parse(self):
+
+        self.response = requests.get('https://www.gismeteo.ru/weather-odintsovo-11938/3-days/', headers=self.headers)
+
+        if self.response.status_code == 200:
+            html_doc = BeautifulSoup(self.response.text, features='html.parser')
+            list_of_values = html_doc.find_all('span', {'class': 'unit unit_temperature_c'})
+            list_of_dates = html_doc.find_all('div', {'class': 'header_item js_head_item frame_4 showed clearfix'})
+            list_of_states = html_doc.find_all('span', {'class': 'tooltip'})
+
+            for temp in list_of_states:
+                text = re.findall(r, str(temp))
+                self.allstates.append(text)
+
+            for day in range(self.DAYS):
+                for temp in range(day * self.DAYS, day * self.DAYS + 4):
+                    self.temperature.append(list_of_values[temp].text)
+                for state in range(day * self.DAYS, day * self.DAYS + 4):
+                    self.states.append(self.allstates[state])
+                self.dates[list_of_dates[day].text] = self.temperature, self.states
+                self.states = []
+                self.temperature = []
+        print(self.dates)
+
+    def data_handler(self, days, moments, states, temperature):
+        data = {}
+        for day in days:
+            for moment in range(4):
+                data[day] = {moments[moment]: [states[moment], temperature[moment]]}
+        return data
+
+
+class ImageMaker:
+    pass
+
+
+class DatabaseUpdater:
+    pass
+
+
+if __name__ == '__main__':
+    parse = WeatherMaker()
+    parse.parse()
